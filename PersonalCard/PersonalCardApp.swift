4de5f5f442fca386673b62eb4470489c81037e5b
ecemozturkk <ecemozturk704@gmail.com>
2023-04-01 16:55:01 +0300

    Initial Commit

---
+++ PersonalCard/PersonalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCardApp.swift
+//  PersonalCard
+//
+//  Created by Ecem Öztürk on 1.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
